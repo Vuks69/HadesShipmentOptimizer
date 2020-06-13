989cb7d7ae865ecc3bf2819e8c72a0098bafccbf
Vuks <Vuks@Poltergeist>
2020-06-13 22:48:05 +0200

    cleaning and refactoring

--- hsopti/transport.py
+++ hsopti/transport.py
@@ -4,38 +4,30 @@ from shipment import Shipment
 class Transport:
     max_cargo: int
     cargo: [Shipment]
-    location: str
 
-    def __init__(self, location: str, max_cargo: int = 5, cargo: [Shipment] = None):
-        self.location = location
+    def __init__(self, max_cargo: int = 5, cargo: [Shipment] = None):
         self.max_cargo = max_cargo
-        if cargo is None:
-            self.cargo = []
+        self.cargo = cargo or []
 
-    def relocate(self, to_where: str) -> (int, int):
-        self.location = to_where
-        ret = self.check_and_delete_shipments(to_where)
-        return ret
-
-    def add_shipment(self, shipment: Shipment) -> bool:
+    def load_shipment(self, shipment: Shipment) -> bool:
         if len(self.cargo) < 5:
             self.cargo.append(shipment)
             return True
         else:
             return False
 
-    def create_shipment(self, dst: str, val: int = None, amount: int = 1) -> None:
-        if amount <= 0:
-            raise ValueError('Amount of shipments to add must be 1 or higher.')
-        for _ in range(amount):
-            self.add_shipment(Shipment(dst, val))
+    # def create_shipment(self, dst: Location, val: int = None, amount: int = 1) -> None:
+    #     if amount <= 0:
+    #         raise ValueError('Amount of shipments to add must be 1 or higher.')
+    #     for _ in range(amount):
+    #         self.add_shipment(Shipment(dst, val))
 
-    def check_and_delete_shipments(self, location: str) -> (int, int):
-        to_delete = [s for s in self.cargo if s.destination == location]
-        value = 0
-        deleted = 0
-        for td in to_delete:
-            value += td.value
-            deleted += 1
-            self.cargo.remove(td)
-        return deleted, value
+    # def check_and_delete_shipments(self, location: str) -> (int, int):
+    #     to_delete = [s for s in self.cargo if s.destination == location]
+    #     value = 0
+    #     deleted = 0
+    #     for td in to_delete:
+    #         value += td.value
+    #         deleted += 1
+    #         self.cargo.remove(td)
+    #     return deleted, value
