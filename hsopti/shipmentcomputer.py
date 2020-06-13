1b53625f984d7c5d89cca3b4bc7a8d8db6f00758
Vuks <Vuks@Poltergeist>
2020-06-13 22:29:31 +0200

    analysis only needs to report total mileage

--- hsopti/shipmentcomputer.py
+++ hsopti/shipmentcomputer.py
@@ -10,5 +10,5 @@ class ShipmentComputer:
 	then to moon A I. If there are no shipments from A to Z, your ship in A could fill up with shipments to A I, fly to
 	Z, have no space to pick up any of the high-paying shipments, and fly back to A I to deliver the shipments from A.
 	"""
-	def analyze_path(self, path: [Location]) -> (int, int):
+	def analyze_path(self, path: [Location]) -> int:
 		pass
